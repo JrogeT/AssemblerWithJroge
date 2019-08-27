f2f847bd9bb3183248790ea5f5cb1bde81c6c1d1
Jroge <jorgerodrigotorrez@gmail.com>
2019-08-26 22:45:33 -0400

    skel.asm

---
+++ Contenido/skel.asm
@@ -0,0 +1,16 @@
+;Guardar el archivo
+;path C:\Ensamblador\MinGW\bin
+;nasm -f win32 -o skel.o skel.asm -l skel.lst
+;gcc -o skel.exe skel.o
+segment .data
+;Variables inicializadas
+
+segment .bss
+;Variables NO inicializadas
+
+segment .text
+global _main
+_main:
+;Codigo
+	mov eax,1
+ret
