f3410eeed20e14653057ee371188de56d267016d
Jroge <jorgerodrigotorrez@gmail.com>
2019-09-01 12:53:11 -0400

    Variables

--- Contenido/Variables/variables.asm
+++ Contenido/Variables/variables.asm
@@ -5,10 +5,17 @@
 segment .data
 ;Variables inicializadas int x = 10;
 x	db	10
+x2	dw	0x000a  	; 00 0a -> 0a 00
+x3	db	0b1010
+x4	dw	0x1234		; 12 34 -> 34 12
+x5	dd	0x12345678	; -> 78 56 34 12
+x6	dw	0x234		; 23 4  -> 4 23  MAL
+					; 02 34 -> 34 02 BIEN 
 
 segment .bss
 ;Variables NO inicializadas int y;
 y	resb	1
+y2	resd	3
 
 segment .text
 global _main
@@ -17,3 +24,8 @@ _main:
 	mov eax,1
 	mov ebx,1
 ret
+
+
+
+
+
