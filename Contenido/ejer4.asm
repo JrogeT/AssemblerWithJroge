4138350caf1683ee9ea66d58c4a54c7ab4a648b3
Jroge <48605375+Jroge@users.noreply.github.com>
2019-10-15 15:18:15 -0400

    asdasdsdaasd

--- Contenido/ejer4.asm
+++ Contenido/ejer4.asm
@@ -1,9 +1,28 @@
 segment .data
-
+array	dd	2,6,7,99,102,1,0
+;array2	= 2,33,34
 segment .bss
+array2 resd	100
 
 segment .text
 global _main
 _main:
-
+	mov ebx,array
+	mov esi,array2
+	mov ecx,7
+ciclo:
+	mov eax,[ebx]
+	mov edi,3
+	cdq
+	div edi
+	cmp edx,0
+	jne noAgregar
+		mov [esi],eax
+		add esi,4
+noAgregar:
+	add ebx,4
+	loop ciclo
 ret
+
+
+
